65afae8b4bcfbbecec9885e2631808d5f9af225a
saiful132 <76720433+saiful132@users.noreply.github.com>
2021-11-20 00:53:08 +0600

    Add files via upload

---
+++ indes.js
@@ -0,0 +1,9 @@
+function myFunc() {
+	var navmenu = document.querySelector(".navmenu");
+
+	if (navmenu.style.display == "none") {
+		navmenu.style.display = "block";
+	} else {
+		navmenu.style.display = "none";
+	}
+}
